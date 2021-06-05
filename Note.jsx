bcde9629ffdd95d739d17208c8cdeaa666e22fa5
SelfieTech <67514928+SelfieTech@users.noreply.github.com>
2021-06-05 22:04:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+ return (
+   <div className="note">
+     <h1>Javascript and React.js</h1>
+     <p>It was an amazing bootcamp , i have learned a new concept no doubts.
+       Bootcamp was quite an amazing part of mine life.
+       Thank You Sir ....
+     </p>
+   </div>
+ );
+}
+
+export default Note;
